93d6eb1d661adf3ef1693e8b261d4ea59fd8d86c
gich2009 <waneneamos@gmail.com>
2023-07-09 04:14:28 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   wrap_parameters format: []
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
     if params[:user_id]
@@ -43,7 +44,11 @@ class ItemsController < ApplicationController
   end
 
   def render_unprocessable_entity exception
-    render json: {errors: exception.record.errors.full_messages }, status: :render_unprocessable_entity
+    render json: {errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found exception
+    render json: {error: "#{exception.model.humanize} not found" }, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, except: [:created_at, :updated_at], include: {items: {except: [:created_at, :updated_at]}}
+  end
+
+
+  def render_not_found exception
+    render json: {error: "#{exception.model.humanize} not found" }, status: :render_not_found
   end
   
 end
