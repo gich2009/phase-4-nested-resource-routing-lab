4a1ed7fec54bb11a14caaad4e1452b0a7a856d0e
gich2009 <waneneamos@gmail.com>
2023-07-09 04:08:20 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,49 @@
 class ItemsController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
+
+    render json: items, except: [:created_at, :updated_at], include: {user: {except: [:created_at, :updated_at]}}
+  end
+
+  def show
+    if params[:user_id]
+      item = User.find(params[:user_id]).items.find(params[:id])
+    else
+      item = Item.find(params[:id])
+    end
+    
+    render json: item, except: [:created_at, :updated_at]
+  end
+
+
+  def create
+    item = Item.create(item_params)
+
+    if params[:user_id]
+      User.find(params[:user_id]).items << item
+    end
+
+    render json: item, except: [:created_at, :updated_at], status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def render_unprocessable_entity exception
+    render json: {errors: exception.record.errors.full_messages }, status: :render_unprocessable_entity
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    render json: user, except: [:created_at, :updated_at], include: {items: {except: [:created_at, :updated_at]}}
   end
-
+  
 end
